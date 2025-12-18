71947a22e6d0431f92765197c5877c03785736e4
user <sooooooobinn@gmail.com>
2025-12-18 16:53:04 +0900

    feat: JWTFilter 작성

--- src/main/java/com/example/gmt_auth/global/jwt/JWTFilter.java
+++ src/main/java/com/example/gmt_auth/global/jwt/JWTFilter.java
@@ -1,4 +1,64 @@
 package com.example.gmt_auth.global.jwt;
 
-public class JWTFilter {
+import com.example.gmt_auth.domain.auth.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    public JWTFilter(JWTUtil jwtUtil, UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String uri = request.getRequestURI();
+        if (uri.startsWith("/auth")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorization.substring(7);
+
+        if (jwtUtil.isExpired(token)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String username = jwtUtil.getUsername(token);
+
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("유저 없음"));
+
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        Authentication authToken =
+                new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        chain.doFilter(request, response);
+    }
 }
