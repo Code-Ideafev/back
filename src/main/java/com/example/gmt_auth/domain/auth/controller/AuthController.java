fcf768b8f380bd5e004952f12b0353effa67fc94
user <sooooooobinn@gmail.com>
2025-12-20 23:39:47 +0900

    feat: api 작성

--- src/main/java/com/example/gmt_auth/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/gmt_auth/domain/auth/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.example.gmt_auth.domain.auth.controller;
 
+import com.example.gmt_auth.domain.auth.dto.JoinDto;
+import com.example.gmt_auth.domain.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody JoinDto joinDto) {
+        authService.join(joinDto.getUsername(), joinDto.getPassword());
+
+        return ResponseEntity.ok()
+                .body(new ApiResponse("success", "회원가입 성공"));
+    }
+
+    record ApiResponse(String status, String message) {}
 }
