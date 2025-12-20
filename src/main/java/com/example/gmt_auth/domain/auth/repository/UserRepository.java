4b30ea0ad7368d3fbdd1fe0aa916685e75ebce24
user <sooooooobinn@gmail.com>
2025-12-20 23:23:51 +0900

    feat: repository 작성

--- src/main/java/com/example/gmt_auth/domain/auth/repository/UserRepository.java
+++ src/main/java/com/example/gmt_auth/domain/auth/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 }
